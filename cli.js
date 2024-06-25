6b8017edf7279cd8a1dc5fead5501619fed03c1d
Pavel Pliuiko <pavelpliuiko@gmail.com>
2024-06-25 11:08:02 -0230

    implement boilerplate

--- cli.js
+++ cli.js
@@ -1,4 +1,26 @@
-console.log('cli')
-const myArgs = process.argv.slice(2);
+const fs = require('fs');
+const {TokenHandler} = require("./src/token");
+const {ConfigHandler} = require("./src/config");
+const {InitHandler} = require("./src/init");
 
-console.log(myArgs)
+const arguments = process.argv.slice(2)
+
+switch (arguments[0]) {
+    case 'init':
+        const initHandler = new InitHandler()
+        break
+    case 'config':
+        const configHandler = new ConfigHandler()
+        break
+    case 'token':
+        const tokenHandler = new TokenHandler()
+        break
+    case '--help':
+        fs.readFile(__dirname + "/usage.txt", (error, data) => {
+            if (error) throw error;
+            console.log(data.toString());
+        });
+        break
+    default:
+        break
+}
---
+++ src/token.js
@@ -0,0 +1,36 @@
+class TokenHandler {
+    handleRequest(argument, {username, phone, email}) {
+        switch (argument) {
+            case '--help':
+                break
+            case '--count':
+                break
+            case '--new':
+                break
+            case '--upd':
+                break
+            case '--search':
+                break
+            default:
+                break
+        }
+    }
+
+    countTokens() {
+
+    }
+
+    createToken() {
+
+    }
+
+    updateToken() {
+
+    }
+
+    searchToken() {
+
+    }
+}
+
+module.exports = {TokenHandler}
